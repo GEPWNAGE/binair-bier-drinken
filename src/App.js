397981fe355e536323f4a798cebd76849dfd2d19
Pieter Kokx <pieter@kokx.nl>
2018-11-12 15:33:51 +0100

    Made initial components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import outline from './logo_beer_no_inside.svg'
+import beer from './logo_beer_nosides.svg'
+
+class Counter extends Component {
+  render() {
+    return <div className="counter">{this.props.num}</div>;
+  }
+}
+
+class Beer extends Component {
+  render() {
+    let height = 40 - 40 * this.props.show;
+    let rect = 'rect(' + height + 'vmin, 40vmin, 40vmin, 0)';
+    return (
+      <div className="beer-parent">
+        <img src={outline} alt='Beer' className="beer outline"/>
+        <img src={beer} alt='Beer' className="beer inside"
+          style={{'clip': rect}}/>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="beer-container">
+            <Beer show={0.33333}/>
+            <Counter num={16}/>
+          </div>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Binair bier drinken!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
