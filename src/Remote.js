43a8cc983e60d0c8a312da7ef6dd73e24556786b
Pieter Kokx <pieter@kokx.nl>
2019-08-31 00:37:47 +0200

    Initial websocket connection

--- src/Remote.js
+++ src/Remote.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Remote = props => <div>Empty</div>;
+let started = false;
+
+const Remote = props => {
+    console.log(props.match.params.handle);
+
+    useEffect(() => {
+        if (started) {
+            return;
+        }
+
+        const ws = new WebSocket('ws://localhost:5000/remote');
+        started = true;
+
+        //ws.onmessage(msg => console.log(msg));
+        console.log(ws);
+        ws.onerror = err => console.log(err);
+        ws.onopen = e => { ws.send("IDENT " + props.match.params.handle); };
+    })
+
+
+
+    return <div>Empty</div>;
+}
 
 export default Remote;
