0e622a0ece32c9e5b4e59ffaa33c7c584643cc45
Pieter Kokx <pieter@kokx.nl>
2019-08-30 16:48:24 +0200

    Added testing files for remote

---
+++ src/Remote.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Remote = props => <div>Empty</div>;
+
+export default Remote;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Remote from './Remote';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = props => (
+    <Router>
+      <Switch>
+        <Route path="/remote/:handle" component={Remote} />
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
